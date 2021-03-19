f29226d90d25fc2f365c27e044c78b6654f0a773
Jakub Sroka <jakubsroka3@gmail.com>
2021-03-20 00:35:30 +0100

    Initial data scrapping

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from twitter_creds import twitter_keys
 auth = tw.OAuthHandler(twitter_keys['consumer_key'], twitter_keys['consumer_secret'])
 auth.set_access_token(twitter_keys['access_token_key'], twitter_keys['access_token_secret'])
 
-api = tw.API(auth)
+api = tw.API(auth, wait_on_rate_limit=True)
 
 # Define the search term and the date_since date as variables
-search_words = "#szczepimysie"
-date_since = "2020-03-18"
+hash_tag = "CovidVaccine"
+query = "#" + hash_tag + " -filter:retweets"
 
 # Collect tweets
 tweets = tw.Cursor(api.search,
-                   q=search_words,
+                   q=query,
                    lang="pl",
-                   tweet_mode='extended',
-                   since=date_since).items(5)
+                   tweet_mode='extended').items(5000)
+
 for t in tweets:
-    print(t._json)
-    # print(t.text)
+    with open("search_results_"+hash_tag+".json", "a", encoding="utf-8") as file:
+        file.write(str(t._json) + "\n")
